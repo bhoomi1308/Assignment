0e523bbfdd1aab0790bebc5b891c4e37e5564d32
Bhoomika S <bhoomi3239@gmail.com>
2021-10-24 20:53:24 +0530

    message

--- Assignment/apps.py
+++ Assignment/apps.py
@@ -10,7 +10,13 @@ json_data = open(r'Desktop/sample.json')
 data1 = json.load(json_data) # deserialises it
 data2 = json.dumps(data1) # json formatted string
 json_data.close()
-def main_page(request):
+def post1(request):
+    if request.method=='POST':
+            received_json_data=json.loads(request.body)
+            return StreamingHttpResponse('it was post request: '+str(received_json_data))
+    return StreamingHttpResponse('it was GET request')
+
+def post2(request):
     if request.method=='POST':
             received_json_data=json.loads(request.body)
             return StreamingHttpResponse('it was post request: '+str(received_json_data))
