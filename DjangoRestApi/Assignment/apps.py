f563c44e09ca6ded057cb573c4c24baa8deaae44
Bhoomika S <bhoomi3239@gmail.com>
2021-10-25 19:55:31 +0530

    Add files via upload

---
+++ DjangoRestApi/Assignment/apps.py
@@ -0,0 +1,23 @@
+from django.apps import AppConfig
+import json
+from django.http import StreamingHttpResponse
+
+
+class Assignment(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+
+json_data = open(r'Desktop/sample.json')   
+data1 = json.load(json_data) # deserialises it
+data2 = json.dumps(data1) # json formatted string
+json_data.close()
+def post1(request):
+    if request.method=='POST':
+            received_json_data=json.loads(request.body)
+            return StreamingHttpResponse('it was post request: '+str(received_json_data))
+    return StreamingHttpResponse('it was GET request')
+
+def post2(request):
+    if request.method=='POST':
+            received_json_data=json.loads(request.body)
+            return StreamingHttpResponse('it was post request: '+str(received_json_data))
+    return StreamingHttpResponse('it was GET request')
